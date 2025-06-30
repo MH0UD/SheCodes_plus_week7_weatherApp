edaa2c3e782994978d79d40259772765b33cd085
Marion_Houdayer <ma.houdayer@gmail.com>
2025-06-30 10:10:18 +0200

    JS & HTML
    JS:
    - add the API link,
    - work on the city search engine
    - display the city name & current temperature for the wanted city
    - add a default city name
    
    HTML
    - remove the fake temp & and the h1.inner HTML

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector(".weather-temperature");
+  let temp = response.data.temperature.current;
+
+  console.log(response.data);
+  let cityElement = document.querySelector(".weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  console.log(cityElement.innerHTML);
+  temperatureElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  // api call
+  apiToken = "3bb10a1f4o479a6e4e2c3cb7at836bff";
+
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiToken}`;
+  console.log(apiURL);
+  axios.get(apiURL).then(updateWeather);
+}
+
 function runSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector(".weather-app-city");
-  cityElement.innerHTML = searchInput.value;
-  console.log(cityElement.innerHTML);
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 console.log(searchFormElement);
 
-apiToken = "3bb10a1f4o479a6e4e2c3cb7at836bff";
 searchFormElement.addEventListener("submit", runSearchSubmit);
+
+searchCity("Munich");
